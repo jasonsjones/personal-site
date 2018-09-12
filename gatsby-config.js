96684991314e2dec075a11afe02ea9b3b17c81e7
Jason Jones <jsjones96@gmail.com>
2018-09-12 10:04:15 -0700

    Update deploy config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  siteMetadata: {
-    title: 'A Web Log',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-remark',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/`
-      }
-    }
-  ]
+    pathPrefix: '/blog',
+    siteMetadata: {
+        title: 'A Web Log'
+    },
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-transformer-remark',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `src`,
+                path: `${__dirname}/src/`
+            }
+        }
+    ]
 };
